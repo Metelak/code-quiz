9e33a51ae62ecf952b43cb5dc3533b64810a42fa
coyote <megan.metelak@gmail.com>
2021-12-19 08:39:16 -0600

    intial correct/wrong response to questions

--- assets/script.js
+++ assets/script.js
@@ -5,32 +5,43 @@ var questionTwo = document.getElementById("question-2");
 var questionThree = document.getElementById("question-3");
 var questionFour = document.getElementById("question-4");
 var questionFive = document.getElementById("question-5");
-var answer = document.querySelector(".btn");
+var answerOne = document.getElementById("btn-group-1");
+var answerTwo = document.getElementById("btn-group-2");
+var answerThree = document.getElementById("btn-group-3");
+var answerFour = document.getElementById("btn-group-4");
+var answerFive = document.getElementById("btn-group-5");
+var correctAnswer = document.querySelector(".correct");
+var wrongAnswer = document.querySelector(".wrong");
 
 startQuizBtn.onclick = function (){
     document.getElementById("quiz-intro").style.display = "none";
     questionOne.style.display = "block";
 };
 //  Question 1 is displayed and user selects an option
-    answer.onclick = function (){
+    answerOne.onclick = function (){
+        if (correctAnswer){
+            alert("Correct!");
+        } else if (wrongAnswer) {
+            alert("Wrong!");
+        };
         questionOne.style.display = "none";
         // Question 2 appears
         questionTwo.style.display = "block";
     };
 // Question 2 is displayed and user selects an option
-    answer.onclick = function (){
+    answerTwo.onclick = function (){
         questionTwo.style.display = "none";
         // Question 3 appears
         questionThree.style.display = "block";
     };
 // Question 3 is displayed and user selects an option
-    answer.onclick = function (){
+    answerThree.onclick = function (){
         questionThree.style.display = "none";
         // Question 4 appears
         questionFour.style.display = "block";
     };
 // Question 4 is displayed and user selects an option
-    answer.onclick = function (){
+    answerFour.onclick = function (){
         questionFour.style.display = "none";
         // Question 5 appears
         questionFive.style.display = "block";
